a187039b7a4622506b7d887af77b464b4e4b4961
Mihai Constantin <constant.mihai@googlemail.com>
2020-02-27 14:05:16 +0100

    Update Zot.groovy

--- src/org/foo/Zot.groovy
+++ src/org/foo/Zot.groovy
@@ -3,6 +3,17 @@ package org.foo
 
 def checkOutFrom(String repo = 'default') {
     echo "Repo is ${repo}"
+    pipeline {
+    agent { docker { image 'python:3.5.1' } }
+    stages {
+        stage('build') {
+            steps {
+                script {
+                    sh 'ls -al'
+                }
+            }
+        }
+    }
 }
 
 return this
